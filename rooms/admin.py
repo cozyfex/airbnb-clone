a07a9a190c311c019ed87a420695433d8f23bd5f
cozyfex <59948728+cozyfex@users.noreply.github.com>
2020-02-13 22:16:29 +0900

    Update admin.py

--- rooms/admin.py
+++ rooms/admin.py
@@ -21,6 +21,7 @@ class PhotoInline(admin.TabularInline):
     model = models.Photo
 
 
+# 관리자 화면에 나오게 등록한다.
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
@@ -28,9 +29,13 @@ class RoomAdmin(admin.ModelAdmin):
 
     inlines = (PhotoInline,)
 
+    # 관리자 화면에 나올 필드들을 영역별로 지정하여 보여 줄 수 있다.
+    # 튜플 내부에서는 '),' 로 끝나야 한다!
     fieldsets = (
         (
+            # 영역 이름
             "Basic Info",
+            # 해당 영역에 표시할 필드
             {
                 "fields": (
                     "name",
@@ -52,6 +57,7 @@ class RoomAdmin(admin.ModelAdmin):
         ("Last Details", {"fields": ("host",)}),
     )
 
+    # 리스트 페이지에서 보여질 필드들을 설정한다.
     list_display = (
         "name",
         "country",
@@ -69,6 +75,7 @@ class RoomAdmin(admin.ModelAdmin):
         "total_rating",
     )
 
+    # 리스트페이지 우측에 필터를 할 필드를 설정한다.
     list_filter = (
         "instant_book",
         "host__superhost",
